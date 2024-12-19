203e4ec891c7d0c56186d1402bf5f7fed05f79a8
Nathaniel Anzo Mugenyi <41405954+anzonathan@users.noreply.github.com>
2024-12-19 17:36:11 +0300

    Update pwm.ino

--- pwm.ino
+++ pwm.ino
@@ -1,19 +1,30 @@
 #define NUM_LEDS 5   // Number of LEDs
 #define POT_PIN A0    // Analog pin connected to the potentiometer
 
-int red = 3;
-int green = 5;
-int blue = 6;
-int yellow = 10;
-int white = 11;
-int ledPins[NUM_LEDS] = {red, green, blue, yellow, white};
+//led number
+int l1 = 8;
+int l2 = 9;
+int l3 = 10;
+int l4 = 11;
+int l5 = 12;
+
+//Sensor Min and Max
+const int Min = 0;
+const int Max = 1023;
+
+//int ledPins[NUM_LEDS] = {l1,l2,l3,l4,l5};
+
 void setup() {
-    // Set all LED pins as outputs
-     pinMode(red, OUTPUT);
-  pinMode(green, OUTPUT);
-  pinMode(blue, OUTPUT);
-  pinMode(yellow, OUTPUT);
-  pinMode(white, OUTPUT);
+  Serial.begin(9600);
+
+  // Set all LED pins as outputs
+  pinMode(l1, OUTPUT);
+  pinMode(l2, OUTPUT);
+  pinMode(l3, OUTPUT);
+  pinMode(l4, OUTPUT);
+  pinMode(l5, OUTPUT);
+
+  //Set Potentiometer as input
   pinMode(A0, INPUT);
 
 }
@@ -21,14 +32,65 @@ void setup() {
 void loop() {
     // Read the potentiometer value (0 to 1023)
     int potValue = analogRead(A0);
-    
-    // Map the potentiometer value to an LED index (0 to NUM_LEDS-1)
-    int ledIndex = (potValue * NUM_LEDS) / 1024;
-    
-  
-    for (int i = 0; i < NUM_LEDS; i++) {
-            analogWrite(ledPins[i], 255);  // Turn on the selected LED
+
+    //interval value for pot 
+    int index = map(potValue, Min, Max, 0,4);
+
+    if (potValue == 0){
+        digitalWrite(l1,LOW);
+        digitalWrite(l2,LOW);
+        digitalWrite(l3,LOW);
+        digitalWrite(l4,LOW);
+        digitalWrite(l5,LOW);
 
     }
-    
+
+    else {
+
+    //Switch Case
+    switch (index){
+      case 0:
+        Serial.println("Soft " + String(potValue));
+        digitalWrite(l1,HIGH);
+        digitalWrite(l2,LOW);
+        digitalWrite(l3,LOW);
+        digitalWrite(l4,LOW);
+        digitalWrite(l5,LOW);
+        break;
+
+      
+      case 1:
+        Serial.println("mid " + String(potValue));
+        digitalWrite(l2,HIGH);
+        digitalWrite(l3,LOW);
+        digitalWrite(l4,LOW);
+        digitalWrite(l5,LOW);
+        break;
+
+      
+      case 2:
+        Serial.println("loud " + String(potValue));
+        digitalWrite(l3,HIGH);
+        digitalWrite(l4,LOW);
+        digitalWrite(l5,LOW);
+        break;
+
+      
+      case 3:
+        Serial.println("louder " + String(potValue));
+        digitalWrite(l4,HIGH);
+        digitalWrite(l5,LOW);
+        break;
+
+      
+      case 4:
+        digitalWrite(l5,HIGH);
+        Serial.println("loudest " + String(potValue));
+        break;
+
+    }
+    delay(500);}
+
+
+
 }
